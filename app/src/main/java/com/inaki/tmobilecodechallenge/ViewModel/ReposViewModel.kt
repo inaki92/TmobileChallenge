ae5e576f585a4a93a6b274f3007932a6996ca063
inaki92 <lizarraga9206@gmail.com>
2019-09-06 15:54:40 -0400

    solved

--- app/src/main/java/com/inaki/tmobilecodechallenge/ViewModel/ReposViewModel.kt
+++ app/src/main/java/com/inaki/tmobilecodechallenge/ViewModel/ReposViewModel.kt
@@ -2,10 +2,10 @@ package com.inaki.tmobilecodechallenge.ViewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.inaki.tmobilecodechallenge.Model.Repositories.RepoModel
 import com.inaki.tmobilecodechallenge.Model.Users.RepoUserModel
 import com.inaki.tmobilecodechallenge.Network.ApiService
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -15,7 +15,7 @@ class ReposViewModel(private val apiService: ApiService): ViewModel() {
     fun getUsersSearched(userName: String): MutableLiveData<RepoUserModel> {
         val users = MutableLiveData<RepoUserModel>()
 
-        viewModelScope.launch {
+        CoroutineScope(Dispatchers.Main).launch {
             val newUser: RepoUserModel = withContext(Dispatchers.IO) {
                 apiService.getSearchedUser(userName).body()!!
             }
@@ -27,7 +27,7 @@ class ReposViewModel(private val apiService: ApiService): ViewModel() {
     fun getAllUsers(): MutableLiveData<List<RepoUserModel>> {
         val allUsers = MutableLiveData<List<RepoUserModel>>()
 
-        viewModelScope.launch {
+        CoroutineScope(Dispatchers.Main).launch {
             val allUser = withContext(Dispatchers.IO){
                 apiService.getUsers().body()
             }
@@ -39,7 +39,7 @@ class ReposViewModel(private val apiService: ApiService): ViewModel() {
     fun getAllReposFromUser(user: String): MutableLiveData<List<RepoModel>> {
         val allRepos = MutableLiveData<List<RepoModel>>()
 
-        viewModelScope.launch {
+        CoroutineScope(Dispatchers.Main).launch {
             val allReposCall = withContext(Dispatchers.IO){
                 apiService.getReposFromUser(user).body()
             }
